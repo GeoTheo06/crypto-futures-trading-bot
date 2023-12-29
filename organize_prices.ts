bdf17959b02048c0b5de0e0d4ae17ccf801b72c1
GeoTheo06 <theodorgeor@gmail.com>
2023-12-29 20:41:01 +0200

    accumulated data (and converted milliseconds to dates) to a file succesfully

---
+++ organize_prices.ts
@@ -0,0 +1,39 @@
+import fs from "fs";
+import moment from "moment";
+import { parse } from "csv-parse/sync";
+import { stringify } from "csv-stringify/sync";
+
+const directoryPath = "price_data";
+
+//getting how many years of data we have (currently 2023 only)
+fs.readdirSync(directoryPath).forEach((file) => {
+	// Read the CSV file
+	const input = fs.readFileSync(`${directoryPath}/${file}`, "utf8");
+
+	// Define a type for the record structure in the CSV file
+	interface Record {
+		open_time: string;
+		close_time: string;
+		[key: string]: string;
+	}
+
+	// Parse the CSV file
+	let records: Record[] = parse(input, {
+		columns: true,
+		skip_empty_lines: true,
+	});
+
+	// Convert epoch to readable format (they are in milliseconds)
+	records.forEach((record) => {
+		record.open_time_readable = moment(parseInt(record.open_time)).format("MM/DD/YYYY, hh:mm:ss A");
+		record.close_time_readable = moment(parseInt(record.close_time)).format("MM/DD/YYYY, hh:mm:ss A");
+	});
+
+	// Convert back to CSV
+	let output = stringify(records, {
+		header: true,
+	});
+
+	fs.appendFileSync("BTCUSDT-5m-ALL-DATA.csv", output);
+});
+console.log("File conversion complete.");
