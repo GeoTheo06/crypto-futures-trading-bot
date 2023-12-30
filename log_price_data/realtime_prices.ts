567d16e02427a3a5d4fd4df89c1ddfcf32ae5b8f
GeoTheo06 <78025790+GeoTheo06@users.noreply.github.com>
2023-12-30 21:19:29 +0000

    gets the last column so that i can add the next data later

--- log_price_data/realtime_prices.ts
+++ log_price_data/realtime_prices.ts
@@ -1,1 +1,15 @@
 //will update price from the last  date in the log.
+import fs from "fs"
+import {parse} from "csv-parse/sync"
+const csvFilePath = "log_price_data/price_data/BTCUSDT-5m-ALL-DATA.csv";
+
+export function saveRealtimePrices() {
+	// Read and parse the CSV file
+	const csvData = fs.readFileSync(csvFilePath, "utf8");
+	const parsedData = parse(csvData, {
+		columns: true, 
+	});
+	// Get the last row in the parsed data
+	const lastRow = parsedData[parsedData.length - 1];
+	
+}
