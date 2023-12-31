45bd6bb9d900e1b2f90bc4fb41c596508326edb6
GeoTheo06 <78025790+GeoTheo06@users.noreply.github.com>
2023-12-31 17:31:02 +0000

    optimized code and only keeping open_time (excluded close_time)

--- log_price_data/historical_prices.ts
+++ log_price_data/historical_prices.ts
@@ -4,57 +4,60 @@ import { parse } from "csv-parse/sync";
 import { stringify } from "csv-stringify/sync";
 
 const directoryPath = "log_price_data/price_data";
-var firstMonth = true;
-fs.unlinkSync("log_price_data/price_data/BTCUSDT-5m-ALL-DATA.csv"); //DELETE AFTER TESTING
-export function saveHistoricalPrices() {
+const outputFile = "BTCUSDT-5m-ALL-DATA.csv";
 
-	// Loop through each file in the directory
+interface BinanceTypeMarketData {
+	open: number;
+	high: number;
+	low: number;
+	close: number;
+	open_time: string;
+}
+
+interface MarketData {
+	open: number;
+	high: number;
+	low: number;
+	close: number;
+	time: string;
+}
+
+// DELETE THIS LINE AFTER TESTING
+try {
+	fs.unlinkSync("log_price_data/price_data/BTCUSDT-5m-ALL-DATA.csv");
+} catch (error) {
+	console.log(`${directoryPath}/${outputFile} doesn't exist`);
+}
+
+export function saveHistoricalPrices() {
+	let allMarketData: MarketData[] = [];
+
+	//iterate over each file
 	fs.readdirSync(directoryPath).forEach((file) => {
 		const input = fs.readFileSync(`${directoryPath}/${file}`, "utf8");
 
-		interface Record {
-			open_time?: string;
-			close_time?: string;
-			volume?: string;
-			quote_volume?: string;
-			count?: string;
-			taker_buy_volume?: string;
-			taker_buy_quote_volume?: string;
-			ignore?: string;
-			[key: string]: string | undefined;
-		}
-
-		let records: Record[] = parse(input, {
+		let monthlyMarketData: BinanceTypeMarketData[] = parse(input, {
 			columns: true,
 			skip_empty_lines: true,
 		});
 
-		records = records.map((record) => {
-			if (record.open_time && record.close_time) {
-				// Convert epoch to readable format if open_time and close_time are defined
-				record.open_time_readable = moment(parseInt(record.open_time)).format("MM/DD/YYYY-hh:mm:ss A");
-				record.close_time_readable = moment(parseInt(record.close_time)).format("MM/DD/YYYY-hh:mm:ss A");
-			}
-
-			// Remove the unecessary fields
-			delete record.open_time;
-			delete record.close_time;
-			delete record.volume;
-			delete record.quote_volume;
-			delete record.count;
-			delete record.taker_buy_volume;
-			delete record.taker_buy_quote_volume;
-			delete record.ignore;
-			return record;
+		monthlyMarketData.forEach((row) => {
+			let processedRow: MarketData = {
+				open: row.open,
+				high: row.high,
+				low: row.low,
+				close: row.close,
+				time: moment(parseInt(row.open_time)).format("YYYY/MM/DDThh:mm:ss A"),
+			};
+			allMarketData.push(processedRow);
 		});
-		//because of the "header: firstMonth" only the header of the first file will be printed in the (beginning of the) final file
-		let output = stringify(records, {
-			header: firstMonth,
-		});
-		firstMonth = false;
-
-		fs.appendFileSync("log_price_data/price_data/BTCUSDT-5m-ALL-DATA.csv", output);
 	});
+
+	const csvOutput = stringify(allMarketData, {
+		header: true,
+	});
+
+	fs.writeFileSync("log_price_data/price_data/BTCUSDT-5m-ALL-DATA.csv", csvOutput);
 
 	console.log("File conversion complete.");
 }
