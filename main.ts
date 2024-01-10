4c0dfa4a703c88e2f21e1361c700ad9ea622f3df
GeoTheo06 <theodorgeor@gmail.com>
2024-01-10 23:50:01 +0200

    main.ts is now main

--- main.ts
+++ main.ts
@@ -1,11 +1,15 @@
 import * as dotenv from "dotenv";
 dotenv.config();
-import { saveHistoricalPrices } from "./save_price_data/historical_prices";
-import { saveRealtimePrices } from "./save_price_data/realtime_prices";
+
 import { calcEma } from "./indicators/calculate_indicators";
+import { save_price_data } from "./save_price_data/save_price_data";
+import mongoose from "mongoose";
+import { Schema, model } from "mongoose";
+import { PriceDataI } from "./price_data_interface";
+
+//connect to binance api
 const Binance = require("node-binance-api");
 
-// Assuming Binance and EMA have appropriate TypeScript definitions
 const binance = new Binance().options({
 	APIKEY: process.env.API_KEY!,
 	APISECRET: process.env.API_SECRET!,
@@ -13,6 +17,28 @@ const binance = new Binance().options({
 	test: false, // For testnet
 });
 
-saveHistoricalPrices();
-saveRealtimePrices();
+//connect to Database
+
+export const priceDataSchema = new Schema<PriceDataI>({
+	timestamp: { type: Number, required: true },
+	readableTime: { type: String, required: true },
+	open: { type: Number, required: true },
+	close: { type: Number, required: true },
+	high: { type: Number, required: true },
+	low: { type: Number, required: true },
+});
+
+//indexes documents using the timestamp in descending order so that the sorting is more efficient
+priceDataSchema.index({ timestamp: -1 });
+
+try {
+	mongoose.connect("mongodb://127.0.0.1:27017/btcusdt");
+	console.log("Connected to MongoDB");
+} catch (err) {
+	console.error("Could not connect to MongoDB", err);
+}
+
+export const PriceData = model<PriceDataI>("PriceData", priceDataSchema);
+
+save_price_data();
 calcEma();
