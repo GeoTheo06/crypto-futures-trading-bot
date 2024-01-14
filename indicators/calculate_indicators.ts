7ec56bc355044661532c331817a1bea660a0c04f
GeoTheo06 <theodorgeor@gmail.com>
2024-01-14 22:47:59 +0200

    minor changes

--- indicators/calculate_indicators.ts
+++ indicators/calculate_indicators.ts
@@ -1,18 +1,21 @@
-import { EMA } from 'technicalindicators';
-import * as fs from 'fs';
-import {parse} from "csv-parse/sync"
+import { EMA } from "technicalindicators";
+import * as fs from "fs";
+import { parse } from "csv-parse/sync";
 
+// export function calcEma() {
+// // Function to read CSV and extract closing prices
+// const readCSV = (filePath: string): number[] => {
+//     const fileContent = fs.readFileSync(filePath, { encoding: 'utf-8' });
+//     const records = parse(fileContent, { columns: true });
+//     return records.map((record: any) => parseFloat(record.close));
+// };
+
+// const filePath = 'log_price_data/price_data/BTCUSDT-5m-ALL-DATA.csv';
+// const closingPrices = readCSV(filePath);
+
+// const test = EMA.calculate({period: 200, values: closingPrices });
+// fs.writeFileSync("fg", test.toString());
+// }
 export function calcEma() {
-// Function to read CSV and extract closing prices
-const readCSV = (filePath: string): number[] => {
-    const fileContent = fs.readFileSync(filePath, { encoding: 'utf-8' });
-    const records = parse(fileContent, { columns: true });
-    return records.map((record: any) => parseFloat(record.close));
-};
-
-const filePath = 'log_price_data/price_data/BTCUSDT-5m-ALL-DATA.csv';
-const closingPrices = readCSV(filePath);
-
-const test = EMA.calculate({period: 200, values: closingPrices });
-fs.writeFileSync("fg", test.toString());
+	console.log("hello");
 }
